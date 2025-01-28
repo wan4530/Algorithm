77655af47aa2d0134028ef04fce0dc30443ff7b8
wan4530 <130379941+wan4530@users.noreply.github.com>
2025-01-29 06:08:12 +0900

    [Silver III] Title: 프린터 큐, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+typedef struct {
+	int number;
+	int important;
+}PR;
+int main(void) {
+	int t;
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		int n, m;
+		PR pr[1001] = { 0 };
+		PR bfr[101] = { 0 };
+		scanf("%d %d", &n, &m);
+
+		int front = 0;
+		int rear = n - 1;
+		int real = 0;
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &pr[j].important);
+			pr[j].number = j;
+		}
+		for(int j = front; j<= rear;j++){
+			int jud = 0;
+			for (int k = j; k <= rear; k++) {
+				if (pr[j].important < pr[k].important) {
+					rear++;
+					front++;
+					pr[rear].important = pr[j].important;
+					pr[rear].number = pr[j].number;
+					jud++;
+					break;
+				}
+			}
+			for (int k = j; k <= rear; k++) {
+				if (pr[j].important < pr[k].important) {
+					jud++;
+					break;
+				}
+			}
+			if (jud == 0) {
+				bfr[real].important = pr[j].important;
+				bfr[real].number = pr[j].number;
+				real++;
+			}
+		}
+		for (int j = 0; j < real; j++) {
+			if (m == bfr[j].number) {
+				printf("%d\n", j + 1);
+			}
+			
+		}
+	}
+	return 0;
+}
