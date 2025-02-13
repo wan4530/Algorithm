a8eec1a5f37c102cf97a6428458574225fd3fe90
wan4530 <130379941+wan4530@users.noreply.github.com>
2025-02-13 18:01:49 +0900

    [Silver II] Title: 스택 수열, Time: 24 ms, Memory: 2092 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int arr[100000];
+int cpy[100000];
+char m[200000];
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int j = 1;
+	int h = 0;
+	int k = 0;
+	cpy[h] = j;
+	m[0] = '+';
+	for (int i = 1; i < 2*n; i++) {
+		//printf("%d\t%d\n", cpy[h], arr[k]);
+		if (arr[k] == cpy[h]) {
+			k++;
+			h--;
+			m[i] = '-';
+		}
+		else {
+			h++;
+			j++;
+			cpy[h] = j;
+			m[i] = '+';
+		}
+	}
+	if (m[(n * 2) - 1] == '+') {
+		printf("NO");
+	}
+	else {
+		for (int i = 0; i < n * 2; i++) {
+			printf("%c\n", m[i]);
+		}
+	}
+	return 0;
+}
