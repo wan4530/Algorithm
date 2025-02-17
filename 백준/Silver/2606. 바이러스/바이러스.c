699c8ece3ac834eb0a57c6f882bf8670b1719d92
wan4530 <130379941+wan4530@users.noreply.github.com>
2025-02-17 17:37:10 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int graph[101][101];
+int dfs[101];
+void f_dfs(int v, int n, int* sum) {
+	dfs[v] = 1;
+	*(sum) += 1;
+	//printf("%d\n", *sum);
+	for (int i = 1; i <= n; i++) {
+		if (graph[v][i] == 1 && dfs[i] == 0) {
+			f_dfs(i, n, sum);
+		}
+	}
+	return ;
+
+}
+int main() {
+	int n,m;
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		scanf("%d%d", &x, &y);
+		graph[x][y] = 1;
+		graph[y][x] = 1;
+	}
+	int ans = 0;
+	int* pans = &ans;
+	f_dfs(1, n, pans);
+	printf("%d", ans - 1);
+}
