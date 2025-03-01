4077c05942020ecf2df8e1ad9c237eb75b4ff6ab
wan4530 <130379941+wan4530@users.noreply.github.com>
2025-03-01 17:52:53 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.c
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -3,16 +3,13 @@ int main(void) {
     int n;
     scanf("%d", &n);
     int i = 2;
-    while (1) {
+    while (n!=1) {
         if (n % i == 0) {
             printf("%d\n",i);
             n /= i;
         }
         else {
             i++;
-        }
-        if (n == 1) {
-            break;
         }
     }
     return 0;
